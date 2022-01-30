14810d47f45dcad4407e88c7fdfd8dad6a3874d7
Douglas Gomes <douglasgommez@gmail.com>
2022-01-30 13:19:44 -0300

    poc to process word using mt specified by file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,27 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
     val mt = MTFactory("C:\\Users\\dougl\\IdeaProjects\\maquina-de-turing\\src\\main\\kotlin\\mt.txt").getMT()
+    val tape = Tape("aaaaaaabbbbbb", mt.whiteSymbol, mt.tapeAlphabet, mt.initialState)
+    mt.transitions.show()
+    while(true){
+//        print(tape.actualState + " ")
+//        println(tape.getActualSymbol())
 
+        println(tape.printTape())
+
+       var transition = mt.getTransition(tape.actualState, tape.getActualSymbol())
+
+       if(transition != null){
+           tape.walkOnTape(transition.direction, transition.symbol)
+           tape.actualState = transition.state
+       }else{
+           println("parei")
+           if(mt.isAcceptable(tape.actualState)){
+               println("ACEITA")
+           }else{
+               println("REJEITA")
+           }
+           break
+       }
+    }
 }
